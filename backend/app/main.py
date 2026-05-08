c17caca3132b78ca2bf1768ca1881ff8916e785b
Sanket Bagad <alencolins@gmail.com>
2026-05-08 18:05:07 +0530

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,7 @@ app = FastAPI(title="Songs Playlist Dashboard", version="1.0.0", lifespan=lifesp
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
